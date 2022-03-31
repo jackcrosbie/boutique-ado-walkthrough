c112ef7d1b19559dccf18e57e63c1861994d9506
jackcrosbie <jack.crosbie@mycit.ie>
2022-03-31 15:33:47 +0000

    updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -21,24 +21,24 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         """ cursor will start in full name field when the page is loaded """
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            """ iterate through form fields adding * to placeholder if required field on form """
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-                """ setting all placeholder attributes to the values above """
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                """ iterate through form fields adding * to placeholder if required field on form """
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                    """ setting all placeholder attributes to the values above """
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             """ remove form field labels as we have placeholders """
             self.fields[field].label = False
