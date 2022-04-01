ef727b01e537edd3791debed62f6269fb1dbd724
jackcrosbie <jack.crosbie@mycit.ie>
2022-04-01 14:35:39 +0000

    Added profile form, views and updated template

---
+++ profiles/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    """ checkout order form """
+    class Meta:
+        model = UserProfile
+        exclude = ('user')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {default_
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+        }
+
+        """ cursor will start in full name field when the page is loaded """
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                """ iterate through form fields adding * to placeholder if required field on form """
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                    """ setting all placeholder attributes to the values above """
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            """ remove form field labels as we have placeholders """
+            self.fields[field].label = False
